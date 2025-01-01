a8e6ff7b4948d03d862a241ae16c66fa69fb49a6
113mant <113mant@gmail.com>
2025-01-02 01:06:32 +0400

    adding data source, chaning base directory for rendering, adding builds to git: yes I know this is bad idea

--- src/GPSLogViz.tsx
+++ src/GPSLogViz.tsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 import ScatterPlot from "./ScatterPlot";
 
 // const csvUrl = "https://gist.github.com/113mant/db17bb51319881eb16730f71adea15a9/raw/5254e0c914765a115e0fb88b27e53ebea8bcea48/points.csv";
-const jsonUrl = "https://gist.github.com/113mant/db17bb51319881eb16730f71adea15a9/raw/5254e0c914765a115e0fb88b27e53ebea8bcea48/points.json";
+// const jsonUrl = "https://gist.github.com/113mant/db17bb51319881eb16730f71adea15a9/raw/5254e0c914765a115e0fb88b27e53ebea8bcea48/points.json";
 // const csvUrl = "https://gist.githubusercontent.com/mbostock/3305937/raw/bb5d3b1a7e1ef2cddc6e8b9b9f2c8b0d1d7f8b7f/points.csv";
 // const jsonUrl = "https://gist.githubusercontent.com/mbostock/3305937/raw/bb5d3b1a7e1ef2cddc6e8b9b9f2c8b0d1d7f8b7f/points.json";
 // const csvUrl = "http://localhost:5500/points.csv";
-// const jsonUrl = "http://localhost:5500/points.json";
+const jsonUrl = "/points.json";
 const width = 800;
 const height = 400;
 const xKey = "lat";
